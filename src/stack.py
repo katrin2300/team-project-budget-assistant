dc915b030aee5a9adb7734b621959d93cc305871
katrin2300 <eslautina865@gmail.com>
2026-05-25 15:05:58 +0300

    Update stack.py

--- src/stack.py
+++ src/stack.py
@@ -1,1 +1,27 @@
-kfnkfjfejfief
+class CategoryStack:
+  def __init__(self, name):
+    """
+    создаёт узел дерева с названием категории
+    у каждого узла есть левый и правый потомок (изначально None)
+    """
+    self.stack = []
+
+def push(self, day, amount, category):
+  """
+
+  """
+  self.stack.append((day, amount, category))
+
+def pop(self):
+  """
+
+  """
+  if not self.stack:
+    return None
+  return self.stack.pop()
+
+def is_empty(self):
+  """
+
+  """
+  return len(self.stack) == 0
